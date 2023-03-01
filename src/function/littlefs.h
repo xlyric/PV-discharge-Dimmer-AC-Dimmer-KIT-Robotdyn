df003d4b05bd6e90d9cb38cfafefe04f92bd4a30
Rémi K <remi.kiragossian@gmail.com>
2023-03-01 11:26:55 +0100

    déplacement topic sauvegarde vers Xlyric/sauvegarde

--- src/function/littlefs.h
+++ src/function/littlefs.h
@@ -126,7 +126,7 @@ void saveConfiguration(const char *filename, const Config &config) {
   /// Publish on MQTT 
   char buffer[1024];
   serializeJson(doc, buffer);
-  client.publish(("sauvegarde/"+ node_id).c_str() ,buffer,  true);
+  client.publish(("Xlyric/sauvegarde/"+ node_id).c_str() ,buffer,  true);
 
   // Close the file
   configFile.close();
