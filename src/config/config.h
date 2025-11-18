bbba6af815876a98cbaf151a68c125c579b69ec4
xlyric <cyril.poissonnier+gitkraken@gmail.com>
2025-11-18 09:25:37 +0100

    correction mises en route cooler préchauffe #79

--- src/config/config.h
+++ src/config/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define VERSION "Version 20251025"
+#define VERSION "Version 20251118"
 #define FS_RELEASE "20251025" // date de la release
 
 constexpr size_t TEMPERATURE_PRECISION=11;
